985e1429bd2b782b1a9ccd650d317315fa3a5592
Alex Zvaruch <number3look@gmail.com>
2023-04-05 19:57:23 +0300

    Improvements to function code

--- app/add/edit-category/add.js
+++ app/add/edit-category/add.js
@@ -1,64 +1,92 @@
-// Функція, що додає новий рядок до таблиці
-function addRow() {
-    var nameInput = document.getElementById("nameInput");
-    var descriptionInput = document.getElementById("descriptionInput");
-  
-    // перевіряємо, що обидва поля заповнені
-    if (nameInput.value === "" || descriptionInput.value === "") {
-      alert("Будь ласка, заповніть обидва поля!");
-      return;
-    }
-  
-    var table = document.getElementById("table");
-    var row = table.insertRow(-1);
-    var rowCount = table.rows.length - 1;
-  
-    // Додаємо значення до нового рядка таблиці
-    row.insertCell(0).innerHTML = rowCount;
-    row.insertCell(1).innerHTML = nameInput.value;
-    row.insertCell(2).innerHTML = descriptionInput.value;
-    row.insertCell(3).innerHTML = "<button onclick='editRow(this)'>Редагувати</button> <button onclick='deleteRow(this)'>Видалити</button>";
-  
-    // Очищаємо поля вводу
-    nameInput.value = "";
-    descriptionInput.value = "";
+let categoryTable = document.getElementById("category-table");
+let saveButton = document.getElementById("save-button");
+let clearButton = document.getElementById("clear-button");
+let searchInput = document.getElementById("search-input");
+
+let categories = [];
+
+function addCategory(name, description) {
+  categories.push({ name: name, description: description });
+  updateTable();
+}
+
+function updateTable() {
+  categoryTable.innerHTML = "";
+  for (let i = 0; i < categories.length; i++) {
+    let row = categoryTable.insertRow(-1);
+    let numberCell = row.insertCell(0);
+    let nameCell = row.insertCell(1);
+    let descriptionCell = row.insertCell(2);
+    let actionsCell = row.insertCell(3);
+
+    numberCell.innerHTML = i + 1;
+    nameCell.innerHTML = categories[i].name;
+    descriptionCell.innerHTML = categories[i].description;
+    actionsCell.innerHTML = `
+      <button class="edit-button" onclick="editCategory(${i})">Редагувати</button>
+      <button class="delete-button" onclick="deleteCategory(${i})">Видалити</button>
+    `;
   }
-  
-  // Функція, що видаляє рядок з таблиці
-  function deleteRow(button) {
-    var table = document.getElementById("table");
-    var rowIndex = button.parentNode.parentNode.rowIndex;
-    table.deleteRow(rowIndex);
-    updateRowCount();
-  }
-  
-  // Функція, що редагує вміст рядка таблиці
-  function editRow(button) {
-    var nameInput = document.getElementById("nameInput");
-    var descriptionInput = document.getElementById("descriptionInput");
-  
-    // Отримуємо значення полів з рядка таблиці
-    var rowIndex = button.parentNode.parentNode.rowIndex;
-    var table = document.getElementById("table");
-    var row = table.rows[rowIndex];
-    var name = row.cells[1].innerHTML;
-    var description = row.cells[2].innerHTML;
-  
-    // Вставляємо значення до полів вводу
-    nameInput.value = name;
-    descriptionInput.value = description;
-  
-    // Видаляємо рядок з таблиці
-    table.deleteRow(rowIndex);
-    updateRowCount();
-  }
-  
-  // Функція, що оновлює значення номерів рядків
-  function updateRowCount() {
-    var table = document.getElementById("table");
-    var rows = table.getElementsByTagName("tr");
-    for (var i = 1; i < rows.length; i++) {
-      rows[i].cells[0].innerHTML = i - 1;
+}
+
+function clearForm() {
+  document.getElementById("category-name").value = "";
+  document.getElementById("category-description").value = "";
+}
+
+function searchTable() {
+  let searchText = searchInput.value.toLowerCase();
+  let rows = categoryTable.getElementsByTagName("tr");
+
+  for (let i = 1; i < rows.length; i++) {
+    let name = rows[i].getElementsByTagName("td")[1].innerHTML.toLowerCase();
+    let description = rows[i].getElementsByTagName("td")[2].innerHTML.toLowerCase();
+    if (name.indexOf(searchText) > -1 || description.indexOf(searchText) > -1) {
+      rows[i].style.display = "";
+    } else {
+      rows[i].style.display = "none";
     }
   }
-  
+}
+
+function editCategory(index) {
+  let category = categories[index];
+  document.getElementById("category-name").value = category.name;
+  document.getElementById("category-description").value = category.description;
+  saveButton.innerHTML = "Оновити";
+  saveButton.onclick = function () {
+    updateCategory(index);
+  };
+}
+
+function updateCategory(index) {
+  let newName = document.getElementById("category-name").value;
+  let newDescription = document.getElementById("category-description").value;
+  categories[index].name = newName;
+  categories[index].description = newDescription;
+  clearForm();
+  updateTable();
+  saveButton.innerHTML = "Зберегти";
+  saveButton.onclick = function () {
+    addCategory(newName, newDescription);
+  };
+}
+
+function deleteCategory(index) {
+  categories.splice(index, 1);
+  updateTable();
+}
+
+saveButton.onclick = function () {
+  let name = document.getElementById("category-name").value;
+  let description = document.getElementById("category-description").value;
+  addCategory(name, description);
+};
+
+clearButton.onclick = function () {
+  clearForm();
+};
+
+searchInput.onkeyup = function () {
+  searchTable();
+};
